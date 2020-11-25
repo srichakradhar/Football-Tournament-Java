c6c3951af04236daee9a8632438e2ff106aa83ce
Git Server Admin <admin@git-server>
2020-11-25 18:57:31 +0000

    Add initial repository

---
+++ src/test/java/com/fresco/tournament/FootBallTouranmentApplicationTests.java
@@ -0,0 +1,22 @@
+package com.fresco.tournament;
+
+import org.json.simple.JSONArray;
+import org.junit.jupiter.api.MethodOrderer.Alphanumeric;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+@TestMethodOrder(Alphanumeric.class)
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class FootBallTouranmentApplicationTests {
+
+}
